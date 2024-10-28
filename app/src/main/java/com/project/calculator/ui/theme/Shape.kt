ac43ec9011eed1e97728a7f2844c742810e4a19d
Pratibha1710 <singhpratibha1710@gmail.com>
2024-10-28 13:29:15 +0530

    Initial commit

---
+++ app/src/main/java/com/project/calculator/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package com.project.calculator.ui.theme
+
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
